8b3755d566f7ba5aa7453ecc5c13b23eb6753e93
james <j.alsop99@hotmail.com>
2021-10-14 11:53:53 +0100

    commenting

--- script.js
+++ script.js
@@ -6,20 +6,26 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-
+    // Perform Amount box validation, focus them on the relevant box
     if (principal <= 0){
         alert("Please enter a number greater than 0.")
         document.getElementById("principal").focus()
         return
     }
 
-
+    // Simple interest calculation (not compounded)
     var interest = principal * years * rate /100;
+
+    // Find the year in *years* time
     var year = new Date().getFullYear() + parseInt(years);
 
+    // Assign the result to the resultElement
     var resultElement = document.getElementById("result");
 
-    resultElement.innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%.\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year;
+    resultElement.innerHTML = "If you deposit <mark>" + principal + 
+        "</mark>,\<br\>at an interest rate of <mark>" + rate + 
+        "%</mark>.\<br\>You will receive an amount of <mark>" + interest + 
+        "</mark>,\<br\>in the year <mark>" + year + "</mark>";
 
 }
         
